ae0f45780c045709a414bf0dc66b491e7366afd0
R A Z i <ryme.care@hotmail.com>
2024-06-12 12:37:57 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,9 @@
+from pyrogram import Client, filters
+from info import API_ID, API_HASH, SESSION
+
+TelegramBot = Client(
+  name="TeleBot",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  session_string=SESSION
+)
