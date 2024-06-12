7d93371ce98f13973183633f33dde3ab27efa988
R A Z i <ryme.care@hotmail.com>
2024-06-12 12:46:51 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,16 @@
 from bot import TelegramBot
 from bot.server.serve import web_server
+import asyncio
+from aiohttp import web
 
-
+async def webrun():
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    site = web.TCPSite(app, "0.0.0.0", 5050)
+    await site.start()
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(webrun())
     TelegramBot.run()
-    
+    loop.run_forever()
