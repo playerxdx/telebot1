4e741948cc17677507d07b685fa20e3e876c3342
R A Z i <119275831+MeeRazi@users.noreply.github.com>
2024-06-20 11:41:50 +0530

    update

--- bot/plugins/command.py
+++ bot/plugins/command.py
@@ -100,6 +100,15 @@ async def deploy_bot(client, message):
 user = {}
 afk_status = {}
 notified_users = set()
+
+@Client.on_message(filters.command("afk", PREFIX) & filters.private & filters.me)
+async def set_afk(client, message: Message):
+    user_id = message.from_user.id
+    if user_id == ADMIN:
+        afk_reason = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else "No reason provided."
+        afk_status[user_id] = afk_reason
+        await message.edit(f"You are now AFK: {afk_reason}")
+        
 @Client.on_message(filters.text & filters.private)
 async def greet_user(client, message: Message):
     user_id = message.from_user.id
@@ -119,11 +128,3 @@ async def greet_user(client, message: Message):
     if user_id != ADMIN and ADMIN in afk_status and user_id not in notified_users:
         await message.reply_text(f"My owner is currently AFK: {afk_status[ADMIN]}")
         notified_users.add(user_id)
-
-@Client.on_message(filters.command("afk", PREFIX) & filters.private & filters.me)
-async def set_afk(client, message: Message):
-    user_id = message.from_user.id
-    if user_id == ADMIN:
-        afk_reason = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else "No reason provided."
-        afk_status[user_id] = afk_reason
-        await message.edit(f"You are now AFK: {afk_reason}")
