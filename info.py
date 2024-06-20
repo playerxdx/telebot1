753a0039c99bc18b47e5c010b3d85377ca1d0497
R A Z i <119275831+MeeRazi@users.noreply.github.com>
2024-06-20 14:06:14 +0530

    update

--- info.py
+++ info.py
@@ -2,9 +2,9 @@ import os
 
 API_ID = os.getenv("API_ID") # Get it from my.telegram.org
 API_HASH = os.getenv("API_HASH") # Get it from my.telegram.org
-SESSION = os.getenv("SESSION") # Pyrogram Session String (Run session.py to get this)
+SESSION = os.getenv("SESSION") # Pyrogram Session String (Run session.py to get)
 GENAI_API_KEY = os.getenv("GENAI_API_KEY") # Get it from https://makersuit.google.com/
 TG_NAME = os.getenv("TG_NAME") # Your Telegram Name (Needed if you wanna use One9word plugins)
 DEPLOY_HOOK = os.getenv("DEPLOY_HOOK") # Get it from render.com if you are hosting this bot to RENDER 
 PREFIX = os.getenv("PREFIX", ".") # Command Prefix 
-ADMIN = int(os.getenv("ADMIN")) # Admin ID
+ADMIN = int(os.getenv("ADMIN")) # Owner User_ID
